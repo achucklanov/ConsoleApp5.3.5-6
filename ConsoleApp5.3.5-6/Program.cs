34ceeb4bfdb42016312e97ae96c2a10c3b447a8f
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-25 23:59:06 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp5.3.5-6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp5._3._5_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3 };
+            BigDataOperation(arr);
+
+            Console.WriteLine(arr[0]);
+
+            static void BigDataOperation(in int[] arr)
+            {
+                arr[0] = 4;
+            }
+        }
+    }
+}
