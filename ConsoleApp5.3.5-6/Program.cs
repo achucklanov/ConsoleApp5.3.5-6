433df539ea0b7ca78d372750d25a61988bd80b82
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-26 21:59:20 +0400

    Изменено в соответствии со скринкастом после задания 5.3.6.

--- ConsoleApp5.3.5-6/Program.cs
+++ ConsoleApp5.3.5-6/Program.cs
@@ -5,13 +5,16 @@
         static void Main(string[] args)
         {
             var arr = new int[] { 1, 2, 3 };
-            BigDataOperation(arr);
+            var data = 5;
+
+            BigDataOperation(arr, ref data);
 
             Console.WriteLine(arr[0]);
 
-            static void BigDataOperation(in int[] arr)
+            static void BigDataOperation(in int[] arr, ref int data)
             {
-                arr[0] = 4;
+                data = 4;
+                arr[0] = data;
             }
         }
     }
