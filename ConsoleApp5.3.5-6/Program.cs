4b1153836de5582c370f8a65e6b6979dd19c81b4
AndreyChuck <a.chucklanov@yandex.ru>
2023-12-25 21:36:47 +0400

    Работа 2 в юните 5.3

--- ConsoleApp5.3.5-6/Program.cs
+++ ConsoleApp5.3.5-6/Program.cs
@@ -10,6 +10,7 @@
             BigDataOperation(arr, ref data);
 
             Console.WriteLine("arr[0] = {0}", arr[0]);
+
             Console.WriteLine("data = {0}", data);
 
             static void BigDataOperation(in int[] arr, ref int data)
