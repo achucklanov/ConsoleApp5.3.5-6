31c3a09460f9b68891d25d1232f64c0779299f96
AndreyChuck <a.chucklanov@yandex.ru>
2023-12-25 22:09:55 +0400

    Работа 3 в юните 5.3

--- ConsoleApp5.3.5-6/Program.cs
+++ ConsoleApp5.3.5-6/Program.cs
@@ -10,14 +10,26 @@
             BigDataOperation(arr, ref data);
 
             Console.WriteLine("arr[0] = {0}", arr[0]);
+            Console.WriteLine("data = {0} \n", data);
 
-            Console.WriteLine("data = {0}", data);
+            GetName(out string name1, out string name2);
+
+            Console.WriteLine(name1);
+            Console.WriteLine(name2);
 
             static void BigDataOperation(in int[] arr, ref int data)
             {
                 data = 4;
                 arr[0] = data;
             }
+
+            static void GetName(out string name1, out string name2)
+            {
+                name2 = "Евгения";
+                Console.WriteLine("Введите имя :");
+                name1 = Console.ReadLine();
+            }
+
         }
     }
 }
